6cf0c7bfa20364b355bda5d11a1eede703ea3de2
Himal Giri <guru@insoftnepal.com>
2024-11-04 10:54:24 +0545

    a

---
+++ InsoftNepalWebsite/Controllers/ClientsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsoftNepalWebsite.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InsoftNepalWebsite/Controllers/OurPortfolioController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsoftNepalWebsite.Controllers
+{
+    public class OurPortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult inSchoolErp()
+        {
+            return View();
+        }
+        public IActionResult tradePlus()
+        {
+            return View();
+        }
+        public IActionResult ezregi()
+        {
+            return View();
+        }
+        public IActionResult hospitalPlus()
+        {
+            return View();
+        }
+        public IActionResult atithiManager()
+        {
+            return View();
+        }
+        public IActionResult schoolExpress()
+        {
+            return View();
+        }        
+        public IActionResult payrollAndHrManagement()
+        {
+            return View();
+        }
+    }
+}
